5660eb695cc44514c91235572ec771d48a33bff4
alikastrati <alikastrati5@gmail.com>
2024-07-28 18:41:39 +0000

    encapsulation of our player object
    OOP concepts for our Player/Sprite, also added a Timer struct which i
    can use for time based actions that i will implement later

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	_ "image/png"
 	"math"
+	"time"
 	// "time"
 	// "github.com/hajimehoshi/ebiten/v2/colorm"
 	// "io/fs"
@@ -30,82 +31,129 @@ func mustLoadImage(name string) *ebiten.Image {
 	return ebiten.NewImageFromImage(img)
 }
 
+// Assets
 var PlayerSprite = mustLoadImage("assets/playerShip1_blue.png")
 
+// Our X, Y coordinates
 type Vector struct {
 	X float64
 	Y float64
 }
 
 type Game struct {
-	playerPosition Vector
+	player *Player
 }
 
-func (g *Game) Update() error {
+// Timer that we can use for time based actions
+type Timer struct {
+	currentTicks int
+	targetTicks  int
+}
+
+type Player struct {
+	position Vector
+	sprite   *ebiten.Image
+}
+
+func NewPlayer() *Player {
+	return &Player{
+		position: Vector{X: 100, Y: 100},
+		sprite:   PlayerSprite,
+	}
+}
+
+func (p *Player) Update() {
+
 	speed := 5.0
 	var delta Vector
 
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		delta.Y = -speed
+		delta.Y += speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		delta.X = -speed
+		delta.X -= speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		delta.X = +speed
+		delta.X += speed
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		delta.Y = +speed
+		delta.Y -= speed
 	}
 
-	// This checks for diagonal movement
+	// Makes diagonal movement slower and the same as the other inputs
 	if delta.X != 0 && delta.Y != 0 {
 		factor := speed / math.Sqrt(delta.X*delta.X+delta.Y*delta.Y)
 		delta.X *= factor
 		delta.Y *= factor
 	}
 
-	g.playerPosition.X += delta.X
-	g.playerPosition.Y += delta.Y
-
-	return nil
+	p.position.X += delta.X
+	p.position.Y -= delta.Y
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (p *Player) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
-	screen.DrawImage(PlayerSprite, op)
+	op.GeoM.Translate(p.position.X, p.position.Y)
+	screen.DrawImage(p.sprite, op)
+}
 
-	// ops := &colorm.DrawImageOptions{}
-	// cm := colorm.ColorM{}
-	//
-	// cm.Translate(1.0, 1.0, 1.0, 0.0)
-	// colorm.DrawImage(screen, PlayerSprite, cm, ops)
+// Creates a NewTimer where the targetTicks is given as a parameter
+// and the rest of it's methods update the currentTicks to match
+// that parameter. Once it does, it resets the currentTicks
+// to the defualt value (0)
+func NewTimer(d time.Duration) *Timer {
+	return &Timer{
+		currentTicks: 0,
+		targetTicks:  int(d.Milliseconds()) * ebiten.TPS() / 1000,
+	}
+}
 
-	// width := PlayerSprite.Bounds().Dx()
-	// height := PlayerSprite.Bounds().Dy()
+func (t *Timer) Update() {
+	if t.currentTicks < t.targetTicks {
+		t.currentTicks++
+	}
+}
+
+func (t *Timer) IsReady() bool {
+	return t.currentTicks >= t.targetTicks
+}
+
+func (t *Timer) Reset() {
+	t.currentTicks = 0
+}
+
+// This function runs at 60 Ticks per Second (TPS)
+func (g *Game) Update() error {
+	// How we use our timer
+	// g.attackTimer.Update()
+	// if g.attackTimer.IsReady() {
+	// 	g.attackTimer.Reset()
 	//
-	// halfW := float64(width / 2)
-	// halfH := float64(height / 2)
+	// 	// Execute something (an attack for example)
+	// }
 	//
-	// op.GeoM.Translate(-halfW, -halfH)
-	// op.GeoM.Rotate(45.0 * math.Pi / 180.0)
-	// op.GeoM.Translate(halfW, halfH)
-	//
-	// screen.DrawImage(PlayerSprite, op)
+	g.player.Update()
+	return nil
 
 }
 
+// This function is used to draw an image on our board/canvas?
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.player.Draw(screen)
+
+}
+
+// This creates the Layout (board/canvas?) where our objects/images will be shown
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
 func main() {
 	g := &Game{
-		playerPosition: Vector{X: 100, Y: 10},
+		player: NewPlayer(),
 	}
 
 	err := ebiten.RunGame(g)
